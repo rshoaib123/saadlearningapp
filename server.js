ada44a65f3944bccb71bb1dfbf627013299cf62f
rshoaib123 <44261409+rshoaib123@users.noreply.github.com>
2025-01-14 13:34:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.post('/api/message', async (req, res) => {
     }
 
     const response = await client.chat.completions.create({
-        messages: message,
+        messages: [message],
         model: "gpt-4o",
         temperature: 1,
         max_tokens: 4096,
